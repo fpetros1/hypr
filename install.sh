21cade86ea6369492b0bf64a3089693628cfae45
Fernando Petros <fernandopetros@gmail.com>
2024-04-25 20:53:13 +0100

    Many configurations, Organized Hyprland Configuration

--- install.sh
+++ install.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+# Set $hypr if empty
+if [ -z "$hypr" ]; then
+    export hypr="$HOME/.config/hypr"
+fi
+
+# Set automatic monitor config
+echo -n "source = ./monitors.auto.conf" > $hypr/hypr-modules/monitors.conf
+
+# Load Environment config in bash/zsh
+SOURCE_COMMAND='source "$hypr/.environment"'
+
+if [ -f "$HOME/.zshrc" ] && [[ -z $(cat "$HOME/.zshrc" | grep "$SOURCE_COMMAND") ]]; then
+    echo -e "\n$SOURCE_COMMAND" | tee -a "$HOME/.zshrc"
+fi
+
+if [ -f "$HOME/.bashrc" ] && [[ -z $(cat "$HOME/.bashrc" | grep "$SOURCE_COMMAND") ]]; then
+    echo -e "\n$SOURCE_COMMAND" | tee -a "$HOME/.bashrc"
+fi
+
+# Create wallpaper config file
+cp "$hypr/wallpaper_folders.example.config.txt" "$hypr/wallpaper_folders.config.txt"
+
 # Install Scripts
 sudo rm "/usr/bin/update-scripts-link" 2> /dev/null
 sudo ln -s $hypr/scripts/update-scripts-link /usr/bin/update-scripts-link
@@ -15,6 +37,7 @@ add_config_folder() {
 add_config_folder "$HOME/.config/nvim"
 add_config_folder "$HOME/.config/kitty"
 add_config_folder "$HOME/.config/xdg-desktop-portal"
+add_config_folder "$HOME/.config/lf"
 
 OLDIFS="$IFS"
 IFS=":"
