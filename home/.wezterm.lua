edf3a71e8067d42f5e535f22c91f50bddde3a46b
Fernando Petros <fernandopetros@gmail.com>
2024-05-10 00:07:07 +0100

    Add precmd in .environment, adjusted padding

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -2,10 +2,10 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.window_padding = {
-    left = 15,
-    right = 15,
-    top = 15,
-    bottom = 15
+    left = 20,
+    right = 20,
+    top = 0,
+    bottom = 20
 }
 
 config.window_background_opacity = 0.85
@@ -15,7 +15,7 @@ config.window_close_confirmation = 'NeverPrompt'
 config.window_decorations = 'NONE'
 config.enable_tab_bar = false
 config.font = wezterm.font_with_fallback { 'undefined', 'JetBrains Mono Nerd Font' }
-config.line_height = 0.90
+config.line_height = 0.9
 config.font_size = 15
 config.enable_kitty_graphics = false
 config.warn_about_missing_glyphs = false
