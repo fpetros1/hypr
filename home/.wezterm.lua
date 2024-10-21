7ad78d9cd8aa8d92178b4fcfb317e4d993230bb5
Fernando Petros <fernandopetros@gmail.com>
2024-10-21 10:24:56 +0100

    make wezterm config for both mac and linux

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -1,44 +1,108 @@
 local wezterm = require 'wezterm'
-local mux = wezterm.mux
-local config = wezterm.config_builder()
 
-config.window_padding = {
-    left = 20,
-    right = 20,
-    top = 20,
-    bottom = 20
-}
+function OS_config()
+    local os = Get_os()
 
-config.window_background_opacity = 0.98
-config.default_cursor_style = 'BlinkingBlock'
-config.cursor_blink_rate = 500
-config.window_close_confirmation = 'NeverPrompt'
---config.window_decorations = 'NONE'
-config.enable_tab_bar = false
-config.font = wezterm.font_with_fallback { 'Hack Nerd Font Mono', 'undefined', 'JetBrains Mono Nerd Font' }
-config.line_height = 1.2
-config.font_size = 12
-config.enable_kitty_graphics = false
-config.warn_about_missing_glyphs = false
-config.front_end = "WebGpu"
+    if os == "GNU/Linux" then
+        return Linux_config()
+    end
 
-config.color_schemes = {
-    ['Kanagawa'] = {
-        foreground = '#DCD7BA',
-        selection_fg = '#DCD7BA',
-        background = '#1F1F28',
-        selection_bg = '#223249',
-        cursor_bg = '#DCD7BA',
-        cursor_fg = '#1F1F28',
-        cursor_border = '#DCD7BA',
+    if os == "Darwin" then
+        return Mac_config()
+    end
+
+    return wezterm.config_builder()
+end
+
+function Get_os()
+    local osname = "Windows"
+    local fh, _ = assert(io.popen("uname -o 2>/dev/null", "r"))
+    if fh then
+        osname = fh:read()
+    end
+
+    return osname
+end
+
+function Linux_config()
+    local config = wezterm.config_builder()
+
+    config.window_padding = {
+        left = 20,
+        right = 20,
+        top = 20,
+        bottom = 20
     }
-}
-config.color_scheme = 'Kanagawa'
-config.enable_wayland = true
 
+    config.window_background_opacity = 0.95
+    config.default_cursor_style = 'BlinkingBlock'
+    config.cursor_blink_rate = 500
+    config.window_close_confirmation = 'NeverPrompt'
+    config.enable_tab_bar = false
+    config.font = wezterm.font_with_fallback {
+        'IosevkaTerm Nerd Font Mono',
+        'Hack Nerd Font Mono',
+        'undefined',
+    }
+    config.line_height = 1.05
+    config.font_size = 14
+    config.enable_kitty_graphics = false
+    config.warn_about_missing_glyphs = false
+    config.front_end = "WebGpu"
+    config.term = "wezterm"
+    config.color_scheme = 'Kanagawa (Gogh)'
+    config.enable_wayland = false
+    config.max_fps = 165
+    config.animation_fps = 165
 
-wezterm.on('gui-startup', function(cmd)
-    mux.spawn_window({ args = { 'launch-or-attach-tmux' } })
-end)
+    return config
+end
 
-return config
+function Mac_Config()
+    local config = wezterm.config_builder()
+    local mux = wezterm.mux
+
+    config.window_padding = {
+        left = 20,
+        right = 20,
+        top = 20,
+        bottom = 20
+    }
+
+    config.window_background_opacity = 0.95
+    config.default_cursor_style = 'BlinkingBlock'
+    config.cursor_blink_rate = 500
+    config.window_close_confirmation = 'NeverPrompt'
+    config.enable_tab_bar = false
+    config.font = wezterm.font_with_fallback {
+        'IosevkaTerm Nerd Font Mono',
+        'Hack Nerd Font Mono',
+        'undefined',
+    }
+    config.line_height = 1.05
+    config.font_size = 15
+    config.enable_kitty_graphics = false
+    config.warn_about_missing_glyphs = false
+    config.front_end = "WebGpu"
+    config.term = "wezterm"
+    config.color_scheme = 'Kanagawa (Gogh)'
+    config.max_fps = 165
+    config.animation_fps = 165
+
+    config.set_environment_variables = {
+        PATH = '/usr/local/bin/:' .. os.getenv('PATH')
+    }
+
+    wezterm.on('gui-attached', function()
+        local workspace = mux.get_active_workspace()
+        for _, window in ipairs(mux.all_windows()) do
+            if window:get_workspace() == workspace then
+                window:gui_window():maximize()
+            end
+        end
+    end)
+
+    return config
+end
+
+return OS_config()
